18e67c178bb459d727df61f595c7704f66ff1f73
Daniel S <danielksstone@gmail.com>
2018-06-18 10:36:08 -0700

    finished functional components prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,32 @@
+import React from 'react';
+import Table from './table';
 
+const students = [
+    {
+        name: 'Peter Parker',
+        course: 'Intro to Photography',
+        grade: 85
+    },
+    {
+        name: 'Bruce Banner',
+        course: 'Psychology',
+        grade: 91
+    },
+    {
+        name: 'Tony Stark',
+        course: 'Mechanical Engineering',
+        grade: 100
+    }
+];
 
+const sgt = () => {
+    return (
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
+
+export default sgt ;
+export {students};
